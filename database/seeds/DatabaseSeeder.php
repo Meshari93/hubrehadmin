b2b5f40f812aa0d85d47dbcbf04680dd763492f7
Meshari93 <almeshari93@gmail.com>
2018-02-24 21:16:13 +0300

    v.13

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Generator  ;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      Faker\Generator $faker
         // $this->call(UsersTableSeeder::class);
         DB::table('users')->insert([
           'id' => '1',
@@ -25,12 +26,33 @@ class DatabaseSeeder extends Seeder
             'created_at' => '2017-12-18 02:07:36',
             'updated_at' => '2017-12-18 02:07:36',
             ]);
+
+
         DB::table('model_has_roles')->insert([
             'role_id' => '1',
             'model_id' => '1',
             'model_type' => 'App\User',
             ]);
 
-            $users = factory(App\User::class, 10000)->create();
+            for ($i=0; $i < 5; $i++) {
+              DB::table('users')->insert([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'email' => $faker->unique()->freeEmail,
+                'password' => $password ?: $password = bcrypt('secret'),
+                'location' => $faker->address,
+                'Ip_address' => $faker->ipv4,
+                'phone_num' => $faker->phoneNumber,
+                'pirth_day' => $faker->dateTime($max = '2000-02-25 08:37:17'),
+                'gender' =>    $faker->randomElement($array = array ('male','female')),
+                'id_nashioty' => $faker->numberBetween($min = 1000000000, $max = 1900000000)  ,
+                'status' =>    $faker->randomElement($array = array ('actev', 'blok','stopped')),
+                'nashioty' => $faker->country ,
+                'remember_token' => str_random(10),
+                  ]);
+            }
+
+
+            // $users = factory(App\User::class, 10000)->create();
      }
 }
